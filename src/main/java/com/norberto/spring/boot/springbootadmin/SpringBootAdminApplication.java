9deed0e40b2a8f3ccffc3dbe41b896bc02ada0a0
NORBERTO MUÑOZ DE LA TEJA <HMUTN840@banorte.com>
2019-11-11 11:48:35 -0600

    Initial commit

---
+++ src/main/java/com/norberto/spring/boot/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,17 @@
+package com.norberto.spring.boot.springbootadmin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+
+}
+
+
---
+++ src/test/java/com/norberto/spring/boot/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norberto.spring.boot.springbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
