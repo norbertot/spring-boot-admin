26d8cf356e362b9abea506cb7d5b96cd76a35454
norberto <norberto.m.delateja@gmail.com>
2019-11-11 17:04:07 -0600

    Changes: - Added EnableDiscoveryClient - Deleted application.properties replaced by application.yml - In pom xml the version number was parametrized. - Junit, eureka, actuator dependencies added to the POM. - Added several parameter to the application properties

--- src/main/java/com/norberto/spring/boot/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/com/norberto/spring/boot/springbootadmin/SpringBootAdminApplication.java
@@ -3,9 +3,11 @@ package com.norberto.spring.boot.springbootadmin;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableAdminServer
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
 public class SpringBootAdminApplication {
 
     public static void main(String[] args) {
